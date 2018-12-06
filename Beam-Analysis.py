9a14f3c2c6a2d66b7e9ff414a9304db92db7a3df
calvinosinga <cosinga@wisc.edu>
2018-12-05 20:51:49 -0600

    Finished the plotting gaussians parts, need to test.

--- Beam-Analysis.py
+++ Beam-Analysis.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from io import open
 from scipy import optimize
 from scipy import stats
-from scipy import integrate
 import os
 
 """
@@ -17,13 +16,14 @@ class Baseline:
         Constructs a baseline object that will contain information on its characteristics.
         """
 
-def main(identifier, timestamps, frequencies, chnno_to_feeds):
+def main(identifier, timestamps, frequencies, chnno_to_feeds, remove_seconds):
     """
     Main runs the program that does the beam analysis. identifier is a String that is the unique identifier for data
     taken on a specific source. (in the case of the M1 2018 data it was 3srcNP). timestamps is a list of hours for the
     data, as a list of Strings. (for M1, the timestamps are 20180101234415 to 20180102014415). Frequencies is a list of
     the frequencies for which there is data, as floats. chnno_to_feeds is a list with two indices, the first being
-    the chnno, with the second being another list of length two that has the pair of feed numbers.
+    the chnno, with the second being another list of length two that has the pair of feed numbers. remove_seconds is
+    a list of the seconds that should be removed, in order to isolate the beam (remove calibration and extraneous data)
     """
     # First step is to create the gaussian and fwhm plots and save them to text files.
     cwd = setup()
@@ -36,32 +36,72 @@ def main(identifier, timestamps, frequencies, chnno_to_feeds):
             #the baseline either wasn't found, or wasn't supposed to be analyzed
         else: #I think the else block only runs if except doesn't
             available_baselines.append(bl)
+            #trying to remove calibrations
+            temp = []
+            for pt in range(len(ampl)):
+                if pt not in remove_seconds:
+                    temp.append(ampl[pt])
+            ampl = temp
+
             #this averages the visibilities in bins of 20 frequencies.
             avg = avg_frequencies(ampl)
             median = get_median_frequencies(frequencies)
-            os.mkdir(cwd+) #TODO: create directory to store this baselines info
-            # TODO: need method to eliminate calibration
-            fit = fit_data(avg)
-
-
-
-
-
-
-
+            os.mkdir(cwd+'/Gaussians/'+string(bl))
+            fit = fit_data(avg, remove_seconds)
+            outfile = open(cwd+'/Gaussians/'+string(bl)+'/gaussians'+string(bl), 'w')
+            #this is the file to write the baselines gaussian data in.
+            #TODO: finish the explanation of what the numbers mean
+            outfile.write(unicode('a*np.exp((-((x-b)/c)**2)/2.0)+d... the numbers are a,b,c,d,' + '\n'))
+            for g in fit.gaussians:
+                outfile.write(unicode(string(g[0])+' '+string(g[1])+' '+string(g[2])+' '+string(g[3])+'\n'))
+            plot_gaussians(fit.times, avg, fit.fits, cwd+'/Gaussians/'+string(bl))
+            #TODO: make error text file
+            #TODO: make fwhm plots
     # Second step is to analyze the data for characteristics and then make the maps for them.
 
-def fit_data(data):
+def plot_gaussians(times, msrd, fit, filepath,baseline):
+    median=get_median_frequencies()
+    for x in range(len(msrd)):
+        plt.plot(times,msrd[x], label='Measured Signal')
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Visibility')
+        plt.plot(times, fit[x], label='Fitted Data')
+        plt.legend()
+        plt.savefig(filepath+'/'+string(baseline)+'_'+string(median[x])+'MHz')
+        plt.gcf().clear()
+        plt.clf()
+
+def fit_data(data, remove_seconds):
     """
     Takes the data, and returns both the gaussian, and the fitted points for the gaussian.
     """
+    initial = [4.0,3550.0,450,40] #is there a way to write this such that its not hard coded
 
-    initial = [4.0,3550.0,450,40]
     fits =[] #keeps the data points for the fits
     gaussians = [] #keeps the parameters for the gaussians
+    times = []
+    errors = [] #if optimize could not find a fit, this list will keep track of itself.
+    for i in range(len(data[1])):
+        if i not in remove_seconds:
+            times.append(i)
+    n=0
+    median=get_median_frequencies()
     for fr in avg:
-        result = optimize.curve_fit(gaussian, times, fr, p0=initial)
-
+        try:
+            result = optimize.curve_fit(gaussian, times, fr, p0=initial)
+        except RuntimeError:
+            errors.append(median[n])
+            gaussians.append([0,0,0,0]) #any flat lines would indicate an issue
+        else:
+            params=result.popt
+            gaussians.append(params)
+            fitted_data=[]
+            for t in times:
+                fitted_data.append(gaussian(t,params[0],params[1],params[2],params[3]))
+            fits.append(fitted_data)
+        finally:
+            n+=1
+    return fits,gaussians,errors,times
 
 def get_median_frequencies(frequencies):
     median = []
@@ -133,7 +173,24 @@ def get(cwd, idf, timestamps, chnno):
         amplitude.append(amp_for_one_second)
     return amplitude
 
-
+def baseline_to_feeds(cwd):
+    """
+    Returns two lists with corresponding Chnno -> feed no. pairs.
+    """
+    Chnfeed = open(cwd+'/dish_bl_order.txt', 'r')
+    lines=list(Chnfeed)
+    feeds = []
+    Chnnos = []
+    for val in range(0, len(lines), 2):
+        lines[val]=str(lines[val][1:-3])
+        lines[val] = lines[val].split()
+        for i in range(len(lines[val])):
+            lines[val][i]=int(lines[val][i])
+        feeds.append(lines[val])
+    for val in range(1, len(lines), 2):
+        lines[val]=int(str(lines[val][:-2]))
+        Chnnos.append(lines[val])
+    return feeds,Chnnos
 
 def get_data_from_file(cwd, idf, start, stop, chnno, i_or_r ):
     """
